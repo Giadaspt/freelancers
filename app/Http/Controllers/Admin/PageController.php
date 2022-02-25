11693bdddc889c5774e598949a796af84cd08803
Giadaspt <saputo.giada@gmail.com>
2022-02-25 16:32:56 +0100

    completati e stampati messaggi

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -3,15 +3,42 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Message;
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PageController extends Controller
 {
-    public function messages(){
+    public function messages(Request $request){
 
-        $messages = Message::all();
+        $messages = $request->all();
 
-        return view('admin.users.messages', compact('messages'));
+        $messageExists = true;
+
+        $user_id = Auth::user()->id;
+
+        $messages = Message::where('user_id', $user_id)->get();
+
+        if(count($messages) <= 0){
+            $messageExists = false;
+        }
+
+
+        // if(!array_key_exists('user_id', $messages){
+
+        //     $messageExists = false
+        // });
+
+        return view('admin.users.messages', compact('messages', 'user_id', 'messageExists'));
     }
+
+    // public function messageExists(){
+    //     $user_message = count($user_id == $message->user_id);
+
+    //     if($user_id == $message->user_id){
+
+    //     }
+    // }
+
 }
--- resources/views/admin/users/messages.blade.php
+++ resources/views/admin/users/messages.blade.php
@@ -3,13 +3,21 @@
 @section('content')
 
   <div class="container">
-    @foreach ($messages as $message )
-      <p>{{ $message->name_sender }}
-        <span>{{ $message->created_at }}</span>
-      </p>
-      <p>{{ $message->email_sender }}</p>
-      <p>{{ $message->text }}</p>
-    @endforeach
+
+    @if ($messageExists)
+      @foreach ($messages as $message )
+
+            <p>{{ $message->name_sender }}
+              <span>{{ $message->created_at }}</span>
+            </p>
+            <p>{{ $message->email_sender }}</p>
+            <p>{{ $message->text }}</p>
+      
+      @endforeach
+      @else
+      <p>Non ci sono messaggi</p>
+    @endif
+
   </div>
   
 @endsection
