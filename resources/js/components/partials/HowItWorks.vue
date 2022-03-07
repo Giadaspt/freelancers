f48bfda6ea03de67cfd4f520b41f38cf9a03d1ef
FedericoCaf <fedetheshield09@gmail.com>
2022-03-07 17:12:49 +0100

    sito-pubblico-ok

---
+++ resources/js/components/partials/HowItWorks.vue
@@ -0,0 +1,32 @@
+<template>
+
+<div class="box-howitworks">
+
+   <h2>Qui c'è il come funziona</h2>
+
+
+</div>
+ 
+</template>
+
+<script>
+export default {
+  name: "HowItWorks",
+
+  methods:{
+
+  }
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+ .box-howitworks{
+   width: 100%;
+   height: 200px;
+   background-color: rgb(51, 44, 11);
+}
+
+</style>
